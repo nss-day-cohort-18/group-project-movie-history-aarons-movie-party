a0976cff2f3066dd86b1dfa98c8ad27b234d2804
Jordan Nelson <jnelsontn@gmail.com>
2017-02-13 11:54:34 -0600

    basic template

---
+++ javascripts/fb-getter.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function getKey() {
+  return {
+    apiKey: "AIzaSyAjNt10LaBGKk5edTtotKiduJmaX4JT4zo",
+    authDomain: "moviehistory-e4b18.firebaseapp.com",
+    databaseURL: "https://moviehistory-e4b18.firebaseio.com"
+  };
+}
+
+module.exports = getKey;
+
+
+/*
+
+<script>
+  // Initialize Firebase
+  var config = {
+    storageBucket: "moviehistory-e4b18.appspot.com",
+    messagingSenderId: "534103746806"
+  };
+  firebase.initializeApp(config);
+</script> 
+*/
