0ed023d7711cc323b2153fe1da6aa4997193d979
Jordan Nelson <jnelsontn@gmail.com>
2017-02-13 13:03:05 -0600

    added files from MHFB example

---
+++ javascripts/db-interaction.js
@@ -0,0 +1,15 @@
+
+function searchOMDB(movie) {
+	return new Promise ( function ( resolve, reject ) {
+		$.ajax({
+			url: `http://www.omdbapi.com/?t=${movie}`
+		}).done(function(movieData){
+			resolve(movieData);
+			console.log(movieData);
+		}).fail(function (error){
+			reject(error);
+		});
+	});
+}
+
+
---
+++ javascripts/user.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let firebase = require('./firebaseConfig'),
+	provider = new firebase.auth.GoogleAuthProvider(),
+	currentUser = null;
+
+firebase.auth().onAuthStateChanged ( function(user) {
+	if (user) {
+		console.log("currentUser logged in: ", currentUser);
+		currentUser = user.uid;
+	} else {
+		currentUser = null;
+		console.log("currentUser not logged in");
+	}
+});
+
+
+function logInGoogle() {
+	return firebase.auth().signInWithPopup(provider);
+
+}
+
+function logOut() {
+	return firebase.auth().signOut();
+}
+
+
+
+function getUser() {
+	return currentUser;
+}
+
+function setUser(val) {
+	currentUser = val;
+}
+
+
+module.exports = {logInGoogle, logOut, getUser, setUser};
