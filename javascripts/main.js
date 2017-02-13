761b484c30e5c8994a61b2822270d7287e5728a3
Jordan Nelson <jnelsontn@gmail.com>
2017-02-13 13:22:07 -0600

    small update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
 let db = require('./db-interaction'),
-    templates = require('./dom-builder'),
-    user = require('./user'),
-    firebase = require('./firebaseConfig');
+   templates = require('./dom-builder'),
+   user = require('./user'),
+   firebase = require('./firebaseConfig');
