28a5b17e1fa48a30f9f4db84c8929df4d425ac51
Jordan Nelson <jnelsontn@gmail.com>
2017-02-13 14:35:05 -0600

    updates js

--- javascripts/db-interaction.js
+++ javascripts/db-interaction.js
@@ -1,6 +1,10 @@
 
  //let firebase = require('./firebaseConfig');
-
+/*
+    apiKey: 'AIzaSyAjNt10LaBGKk5edTtotKiduJmaX4JT4zo',
+    authDomain: 'moviehistory-e4b18.firebaseapp.com',
+    databaseURL: 'https://moviehistory-e4b18.firebaseio.com'
+ */
 // ****************************************
 // DB interaction using Firebase REST API
 // ****************************************
@@ -8,13 +12,66 @@
 function searchOMDB (movie) {
 	return new Promise ( function ( resolve, reject ) {
 		$.ajax({
-			url: `http://www.omdbapi.com/?t=${movie}`
+			url: `http://www.omdbapi.com/?t=${movie}`,
+			method: 'get'
 		}).done(
 			function (movieData) {
 			resolve(movieData);
-			console.log(movieData);
 		}).fail(function (error){
 			reject(error);
+		});
+	});
+}
+
+/*
+ * Get the User's Movie List
+ */
+function getMovies (user) {
+	return new Promise(function(resolve, reject) {
+		$.ajax({
+			url: `https://moviehistory-e4b18.firebaseio.com/movies.json?orderBy="uid"&equalTo="${user}"`
+		}).done(function(movieData){
+			resolve(movieData);
+		}).fail( function(error){
+			reject(error);
+		});
+	});
+}
+
+function addMovie (movieFormObj) {
+	console.log('add movie', movieFormObj);
+	return new Promise ( function (resolve, reject ) {
+
+		$.ajax ( {
+			url: 'https://moviehistory-e4b18.firebaseio.com/movies.json',
+			type: 'POST',
+			data: JSON.stringify(movieFormObj),
+			dataType: 'json'
+		}).done(function (movieId) {
+			resolve();
+		});
+	});
+}
+
+function deleteMovie (movieId) {
+	return new Promise ( function ( resolve, reject ) {
+		$.ajax({
+			url: `https://moviehistory-e4b18.firebaseio.com/movies/${movieId}.json`,
+			method: 'DELETE'
+		}).done( function () {
+			resolve();
+		});
+	});
+}
+
+function getMovie (movieId) {
+	return new Promise (function (resolve, reject) {
+		$.ajax ({
+			url: `https://moviehistory-e4b18.firebaseio.com/movies/${movieId}.json`,
+		}).done(function (movieData) {
+			resolve(movieData);
+		}).fail( function (error) {
+			reject (error);
 		});
 	});
 }
