48c20ba768f153d45d3c9cca487069a3b9cb9635
Jordan Nelson <jnelsontn@gmail.com>
2017-02-14 14:40:38 -0600

    getting somewhere

--- javascripts/dom-builder.js
+++ javascripts/dom-builder.js
@@ -1,29 +1,31 @@
 'use strict';
 
 function makeMovieList (movie) {
-	 let $movieDisplay = 
-	$(`<div class="col-xs-4">
-		<ul class="movie-list">
-		</ul>
-		</div>`);
-	$(".movieWrapper").html($movieDisplay);
- 
- 	let movieWrapper = $('#moviewrapper');
+	var movierow = document.getElementById('movierow');
 	for (var key in movie) {
 		let movieList = movie[key];
 		for (var x in movieList) {
-			// console.log(movieList[x].title);
-			let toWebpage = '';
-			toWebpage = `
-						<div id="${movieList[x].id}">
-							${movieList[x].title}<br>
-							${movieList[x].release_date}<br>
-							${movieList[x].poster_path}<br>
-							${movieList[x].overview}<br>
-						</div>
-						`;
-			movieWrapper.append(toWebpage);
-			//console.log(movieList);
+			movierow.innerHTML += 
+							`
+							<div class="mainmoviecard col-md-3">
+							<div id="${movieList[x].id}" class="moviecard">
+								<p id="title">${movieList[x].title}</p>
+								<p id="release_date">${movieList[x].release_date}</p>
+								<p id="overview">${movieList[x].overview}</p>
+							</div>
+							<section class="moveleft"><a href="#" class="watchlist">
+							Add to Watchlist</a></section></div>`;
+/*
+							`
+							<div class="mainmoviecard col-md-3">
+							<div id="${movieList[x].id}" class="moviecard">
+								<b>Title:</b> ${movieList[x].title}<br>
+								<b>Release Date:</b> ${movieList[x].release_date}<br>
+								<b>Description:</b><br>${movieList[x].overview}<br><br>
+							</div>
+							<section class="moveleft"><a href="#" class="watchlist">
+							Add to Watchlist</a></section></div>`;
+*/
 		}
 
 	}
--- javascripts/main.js
+++ javascripts/main.js
@@ -6,41 +6,62 @@ let db = require('./db-interaction'),
    	user = require('./user'),
    	firebase = require('./firebaseConfig');
 
-/*
-function loadMoviesToDOM (movieInfo) {
-	let movieDivCard = document.createElement('div');
-	movieDivCard.innerHTML = movieTemplate(movieInfo);
-	$('.movie-container').append(movieDivCard);
-} */
-
 
 function loadMoviesToDOM() {
   //console.log("Need to load some songs, Buddy");
   let currentUser = user.getUser();
   db.getMovies(currentUser)
   .then(function(movieData){
-    console.log("got data", movieData);
-   /*  var idArray = Object.keys(movieData);
+    // console.log("got data", movieData);
+   /* var idArray = Object.keys(movieData);
     idArray.forEach(function(key){
       movieData[key].id = key;
-    }); */
-    console.log("movie object with id: ", movieData);
-    // templates.makeSongList(songData);
+    }); 
+    console.log("movie object with id: ", movieData); 
+    templates.makeMovieList(movieData); */
   });
 
 }
 
-loadMoviesToDOM();
-
 $('#searchmovies').keypress(function (event) {
 	if (event.which == 13) {
 		let movieSearchInput = document.getElementById('searchmovies').value;
-		console.log(movieSearchInput);
 		db.searchOMDB(movieSearchInput)
     .then( function(resolve) {
-      // console.log(resolve);
       templates.makeMovieList(resolve);
     });
 	}
-
 });
+
+$('#login-btn').click(function() {
+  console.log('clicked auth');
+  user.logInGoogle()
+  .then( function(result){
+    console.log('result from login', result.user.uid);
+    user.setUser(result.user.uid);
+   // $('#auth-btn').addClass('is-hidden');
+   // $('#logout').removeClass('is-hidden');
+    // loadMoviesToDOM();
+  });
+});
+
+
+// Send newSong data to db then reload DOM with updated song data
+$(document).on("click", ".watchlist", function() {
+  let movieObj = buildMovieObj();
+  db.addMovie(movieObj)
+  .then( function(movieId){
+    console.log(movieId);
+   // loadMoviesToDOM();
+  });
+});
+
+function buildMovieObj() {
+    let className = $('.moviecard').attr('id');
+    let movieObj = {
+      title: $(className).html(),
+      uid: user.getUser()
+  };
+  return movieObj;
+}
+
