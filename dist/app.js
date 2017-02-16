a8d7c7fdfba1379447755581f78099b5dc706391
Gilberto Diaz <diaz.gilberto@gmail.com>
2017-02-16 09:15:21 -0600

    add conditional for missing images from api to dom-builder

--- dist/app.js
+++ dist/app.js
@@ -100,17 +100,16 @@ let cardMovieTemplate = function(movie) {
     return new Promise(function(resolve, reject) {
 
         let cardItems = {
-        		movieId: movie.id,
-            image: movie.poster_path ? `${movie.poster_path}` : `${"../dist/images/image-not-available.jpg"}`,
+            movieId: movie.id,
+            image: movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : './dist/images/image-not-available.jpg',
             title: movie.title,
             year: movie.release_date.slice(0, 4),
             myRatings: movie.ratings,
             popularity: Math.round(movie.popularity)
         };
-        let cardTemplate = `
-    												<div class="col-sm-6 col-md-4" data-movieId="${cardItems.movieId}">
+        let cardTemplate = `<div class="col-sm-6 col-md-4" data-movieId="${cardItems.movieId}">
                               <div class="thumbnail">
-                                <img src="https://image.tmdb.org/t/p/w500${cardItems.image}" alt="Movie image ${cardItems.title}">
+                                <img src="${cardItems.image}" alt="Movie image ${cardItems.title}">
                                 <div class="caption">
                                   <h3>${cardItems.title}</h3>
                                   <h3>${cardItems.year}</h3>
@@ -178,7 +177,7 @@ function movieObjToFirebase(movieObj) {
     title: movieObj.original_title,
     year: movieObj.release_date.slice(0, 4),
     overview: movieObj.overview,
-    poster: movieObj.poster_path,
+    poster_path: movieObj.poster_path,
 
     uid: user.getUser()
   };
