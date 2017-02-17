7ccea11be13d6b7595ac88e09befc5fbe65cbf0f
Adam Myers <Tavern@adams-mac.attlocal.net>
2017-02-16 23:53:03 -0600

    only fb calls when leaving selected movie

--- javascripts/dom-builder.js
+++ javascripts/dom-builder.js
@@ -3,19 +3,22 @@
 let cardMovieTemplate = function(movie) {
     return new Promise(function(resolve, reject) {
 
+ 
         //setting up structure for apending items to DOM
         let cardItems = {
+            movieIndex: movie.index ? movie.index : '',
             movieId: movie.id,
             image: movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : './dist/images/image-not-available.jpg',
             title: movie.title,
             year: movie.release_date.slice(0, 4),
             myRatings: movie.ratings,
             popularity: Math.round(movie.popularity),
-            tracked: movie.uid ? 'movie-tracked' : ''
+            tracked: movie.uid ? 'movie-tracked' : '',
+            rated: movie.ratings ? 'movie-rated' : '',
         };
         //card-template
-        let cardTemplate = `<div class="col-sm-6 col-md-4" data-movieId="${cardItems.movieId}">
-                              <div class="thumbnail ${cardItems.tracked}">
+        let cardTemplate = `<div class="col-sm-6 col-md-4">
+                              <div class="thumbnail ${cardItems.tracked} ${cardItems.rated}" data-movieId="${cardItems.movieId}" id=${cardItems.movieIndex}>
                                 <img src="${cardItems.image}" alt="Movie image ${cardItems.title}">
                                 <div class="caption">
                                   <h3>${cardItems.title}</h3>
@@ -36,8 +39,9 @@ let cardMovieTemplate = function(movie) {
                                     <span class="glyphicon glyphicon-star-empty" aria-hidden="true" data-star="9"></span>
                                     <span class="glyphicon glyphicon-star-empty" aria-hidden="true" data-star="10"></span>
                                 </div>
-                                <div class="checkbox">
-                                    <label><input type="checkbox" id="untrack">Untrack this movie</label>
+                                <div class="buttons">
+                                    <button class="track" type="button" id="track">Track this movie</button>
+                                    <button class="untrack" type="button" id="untrack">Untrack this movie</button>
                                 </div>
                               </div>
                             </div>`;
